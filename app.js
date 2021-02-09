d5fee2f6e240c2f803a0b4cead54532408ece410
Rajat Jha <rajatjha30@gmail.com>
2021-02-09 16:46:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,14 +36,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.ejs'));
-});
-
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");

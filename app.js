97cf0d4d72405437c37bda935c8d54a4e42174d0
Rajat Jha <rajatjha30@gmail.com>
2021-02-09 16:39:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', 'index.ejs'));
 });
 
 app.use(function(req, res, next){
